c0d34b46649ccfd2086de4bd74b075be2c9aac70
Saket Kumar Sinha <85068024+saketsinha1209@users.noreply.github.com>
2023-08-07 15:24:42 +0530

    Update board.js

--- src/pages/board.js
+++ src/pages/board.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Header from "@/components/header";
 import { Button, FormControl,FormControlLabel,FormLabel,RadioGroup,Radio } from "@mui/material";
-import PuzzleSlicer from "./Components/puzzleslicer";
+import PuzzleSlicer from "./Components/PuzzleSlicer";
 const Board = () => {
   const router = useRouter();
   const { query } = router;

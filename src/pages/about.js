2bf861046dd215154bce8e84151450b8fe23fe39
saketsinha1209 <85068024+saketsinha1209@users.noreply.github.com>
2023-08-07 14:53:57 +0530

    about change

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,41 +3,6 @@ export default function About() {
     <div className="about" style={{ height: "100%" }}>
       <div className="about-text">
         <h2 className="about-title">About me</h2>
-         As a passionate and determined individual in
-        the field of web development. I am currently working as a solo MERN
-        (MongoDB Express.js, React.js Node.js) web developer. With a strong
-        desire to learn and grow, I am actively seeking job opportunities to
-        apply my skills and contribute to exciting projects. Despite being a
-        beginner, I have invested significant time and effort into mastering the
-        MERN stack. I have completed various online courses, engaged in
-        self-study, and worked on personal projects to enhance my knowledge and
-        practical skills in each component of the stack. In terms of frontend
-        development, I am proficient in React.js, the popular JavaScript library
-        for building user interfaces. I have hands-on experience in creating
-        interactive and responsive websites using React&apos;s component-based
-        approach. I am also familiar with HTML, CSS, and JavaScript, which are
-        the foundational languages for frontend development. On the backend
-        side, I have a solid understanding of Node.js and Express.js. I have
-        built RESTful APIs and utilized server-side logic to handle data
-        processing and database management. While my experience primarily
-        revolves around MongoDB, I am open to learning and working with other
-        databases as well. As a solo developer, I have developed strong
-        problem-solving and time management skills. I am capable of handling the
-        entire development cycle, from requirements gathering and wireframing to
-        deployment and maintenance. I am also proficient in using Git for
-        version control, allowing for efficient code management and
-        collaboration. While I may still be at the beginning of my professional
-        journey, I am highly motivated, adaptable, and eager to learn from
-        experienced developers. I am open to joining a team where I can
-        contribute my skills while also gaining valuable mentorship and
-        guidance. I am confident in my ability to quickly grasp new concepts and
-        technologies, making me a valuable asset to any project. I am actively
-        seeking job opportunities that allow me to apply my MERN skills and grow
-        as a web developer. I am eager to contribute to innovative projects,
-        learn from experienced professionals, and make a positive impact in the
-        industry. If you have any opportunities available or would like to
-        discuss how I can contribute to your project, I would be thrilled to
-        connect with you.
       </div>
       <div className="abouticon">
         <img src="/mern.png" id="mern"></img>

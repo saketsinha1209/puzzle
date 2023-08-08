e61f462c4639ae183f35afa060f85f97980ca3df
Saket Kumar Sinha <85068024+saketsinha1209@users.noreply.github.com>
2023-08-08 13:02:18 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ export default function About() {
     <div className="about" style={{ height: "100%" }}>
       <div className="about-text">
         <h2 className="about-title">About me</h2>
+  <p>I am a passionate full stack web developer with a keen interest in crafting dynamic and innovative digital experiences. Proficient in both frontend and backend technologies, I thrive on bringing ideas to life through code. My journey is fueled by a desire to continuously learn and adapt to the ever-evolving landscape of web development. With a strong foundation in HTML, CSS, JavaScript, and various frameworks, I relish the challenge of translating concepts into functional, user-centric applications. By merging creativity and technical prowess, I am dedicated to creating impactful projects that push boundaries and elevate the online realm.</p>
       </div>
       <div className="abouticon">
         <img src="/mern.png" id="mern"></img>

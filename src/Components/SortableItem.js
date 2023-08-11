7d6ff3e679d7ca45c3f19b8f20ebc28a7c328f3e
Saket Kumar Sinha <85068024+saketsinha1209@users.noreply.github.com>
2023-08-12 02:12:08 +0530

    Update SortableItem.js

--- src/Components/SortableItem.js
+++ src/Components/SortableItem.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
-import Items from "@/Components/items";
+import Items from "@/Components/Items";
 
 const SortableItem = ({id, imageUrl,rows,columns, ...props }) => {
   const {
